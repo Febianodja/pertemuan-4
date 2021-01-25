1fb8c06c4897ed8237e96e33423b9aa9fe605fbe
febianodja <febianodja@gmail.com>
2021-01-25 21:00:08 +0700

    add number 2 & 3

--- function/index.js
+++ function/index.js
@@ -8,18 +8,28 @@ console.log(greet(buyer,items));
 
 // LATIHAN 2---------------
 function helper(number){
-
+    return number - 32;
 }
 
 function getCelcius(fahrenheit){
-    return helper(fahrenheit) - 32;
+    return helper(fahrenheit) * (5/9);
 }
-
-
+console.log(getCelcius(90));
 
 // LATIHAN 3---------------
-
-
+let result = 0;
+function catAge(num){
+    if(num === 1){
+        return result = 15;
+    } else if( num === 2){
+        return result = 24;
+    } else if (num > 2){
+        return result = 24 + ((num-2) * 4);
+    } else {
+        return result;
+    }
+}
+console.log(catAge(5));
 
 
 // LATIHAN 4---------------

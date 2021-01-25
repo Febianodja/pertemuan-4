eb940cdd9eb41a3eff1fc9a0ae7c92e2cab777d9
febianodja <febianodja@gmail.com>
2021-01-25 16:01:12 +0700

    tambah tugas

---
+++ function/index.js
@@ -0,0 +1,30 @@
+// LATIHAN 1---------------
+let buyer = "febian";
+let items = "donat";
+function greet(buyer, items){
+    return (`Terima kasih ${buyer} telah membeli produk ${items}`)
+}
+console.log(greet(buyer,items));
+
+// LATIHAN 2---------------
+function helper(number){
+
+}
+
+function getCelcius(fahrenheit){
+    return helper(fahrenheit) - 32;
+}
+
+
+
+// LATIHAN 3---------------
+
+
+
+
+// LATIHAN 4---------------
+let smallString = (randomStr) => {return randomStr.toLowerCase()};
+console.log(smallString("JFAOIJDAIosjadjaiodjaIOADJS"));
+
+
+
